b541534003f76fbd0d05fda12a0eaa6139e38387
MSI\kev <kevinjamesmayer@gmail.com>
2017-04-10 15:47:44 -0500

    added experience and level up functionality

--- Dingerson/Assets/Scripts/Character/Character.cs
+++ Dingerson/Assets/Scripts/Character/Character.cs
@@ -13,14 +13,21 @@ public class Character {
     public int ExampleInt { get { return _exampleInt; } set { _exampleInt = value; } }
 
     private int _experience;
-    public int Experience { get { return _experience; } set { _experience = value; } }
+    public int Experience { get { return _experience; } set { _experience = value; CheckLevelUp(); } }
 
-    ///For the experience property add an additional funciton call to the set block called CheckLevelUp.  
-    ///Then write that function in this class.  It should check the experience of the Character against a level up barrier, then increment
-    ///the characters level if applicable
+   
     
     public void LevelUp()
     {
 
+    }
+
+    public void CheckLevelUp()
+    {
+        if (_experience >= 100)
+        {
+            Level++;
+            _experience = 0;
+        }
     }
 }
